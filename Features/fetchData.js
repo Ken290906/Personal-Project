e42b0f58bfc818da0866259e7985b325f2389cf7
Huân Nè <112363749+Ken290906@users.noreply.github.com>
2024-07-30 22:38:57 +0700

    Day 7
    Mới fetch data lên ae từ từ nhé

---
+++ Features/fetchData.js
@@ -0,0 +1,26 @@
+//get Data
+
+
+
+async function fetchData() {
+  try {
+    const response = await fetch("https://api.escuelajs.co/api/v1/products");
+
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchData();
+
+// fetch("https://api.escuelajs.co/api/v1/products")
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error))
+
